619360706c4491e5a6f59d20bb964dc1d8fde8e1
Vania <aktives685@gmail.com>
2023-11-30 00:26:41 +0200

    final changes

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,12 @@
 const modal = document.querySelector(".backdrop");
-const modalBtnOpen = document.querySelector(".modal-btn-open");
+const modalBtnOpenList = document.querySelectorAll(".modal-btn-open");
 const modalBtnClose = document.querySelector(".modal-btn-close");
+
 const toggleModal = () => modal.classList.toggle("is-hidden");
-modalBtnOpen.addEventListener("click", toggleModal);
+
+// Добавляем обработчик для каждой кнопки с классом .modal-btn-open
+modalBtnOpenList.forEach((btn) => {
+  btn.addEventListener("click", toggleModal);
+});
+
 modalBtnClose.addEventListener("click", toggleModal);
